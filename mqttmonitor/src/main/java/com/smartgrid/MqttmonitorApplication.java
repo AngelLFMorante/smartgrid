d8a14b2beaba2da615761523f433c18f44b57334
Angel Fm <angellfmorante@gmail.com>
2025-04-15 12:12:30 +0200

    enlace repo smartgrid

---
+++ mqttmonitor/src/main/java/com/smartgrid/MqttmonitorApplication.java
@@ -0,0 +1,13 @@
+package com.smartgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqttmonitorApplication.class, args);
+	}
+
+}
